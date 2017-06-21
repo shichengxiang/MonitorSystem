6f1bb0098fec14a0da55edc79d8f5419b7c40923
shichengxinag <shichengxiang@zhongjinlianhe.com>
2017-06-21 13:28:23 +0800

    通知修改

--- app/src/main/java/com/example/shichengxinag/monitorsystem/ui/notification/NotificationListActivity.java
+++ app/src/main/java/com/example/shichengxinag/monitorsystem/ui/notification/NotificationListActivity.java
@@ -1,9 +1,18 @@
 package com.example.shichengxinag.monitorsystem.ui.notification;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.view.GestureDetectorCompat;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.ScaleAnimation;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 
 import com.example.shichengxinag.monitorsystem.R;
 import com.example.shichengxinag.monitorsystem.ui.BaseActivity;
@@ -15,9 +24,11 @@ import butterknife.BindView;
  * Created by shichengxinag on 2017/6/20.
  */
 
-public class NotificationListActivity extends BaseActivity {
+public class NotificationListActivity extends BaseActivity{
     @BindView(R.id.toolbar)
     View toolbar;
+    PopupWindow mPopupWindow;
+    GestureDetectorCompat mGestureDetector;
 
     @Override
     public int getLayout() {
@@ -26,6 +37,7 @@ public class NotificationListActivity extends BaseActivity {
 
     @Override
     public void init(Bundle savedInstanceState) {
+        initGesture();
     }
 
     @Override
@@ -42,8 +54,36 @@ public class NotificationListActivity extends BaseActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                DialogUtils.showPopWindow(NotificationListActivity.this, toolbar);
+                displayPopWindow();
             }
-        }, 1000);
+        }, 400);
+    }
+
+    void initGesture() {
+        mGestureDetector = new GestureDetectorCompat(this,new MyGestureListener());
+    }
+
+    void displayPopWindow() {
+        if (mPopupWindow == null) {
+            View contentView = LayoutInflater.from(this).inflate(R.layout.layout_topfilter, null);
+            mPopupWindow = new PopupWindow(contentView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+            mPopupWindow.setAnimationStyle(R.style.popwindow_anim);
+            mPopupWindow.setTouchable(true);
+            mPopupWindow.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00000000")));
+        }
+        if (!mPopupWindow.isShowing())
+            mPopupWindow.showAsDropDown(toolbar);
+    }
+    class MyGestureListener extends SimpleOnGestureListener{
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Log.d("===",e.getY()+"位置");
+            return super.onDown(e);
+        }
     }
 }
