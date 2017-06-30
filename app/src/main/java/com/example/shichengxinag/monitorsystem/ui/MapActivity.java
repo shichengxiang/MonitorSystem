b71686a7f6855b18bd65f9d3c89416d1533f78c3
shichengxinag <shichengxiang@zhongjinlianhe.com>
2017-06-30 18:43:27 +0800

    1.5.1commit

--- app/src/main/java/com/example/shichengxinag/monitorsystem/ui/MapActivity.java
+++ app/src/main/java/com/example/shichengxinag/monitorsystem/ui/MapActivity.java
@@ -1,6 +1,13 @@
 package com.example.shichengxinag.monitorsystem.ui;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
@@ -9,16 +16,20 @@ import com.example.shichengxinag.monitorsystem.R;
 import com.example.shichengxinag.monitorsystem.presenter.MapPresenter;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by shichengxinag on 2017/6/16.
  */
 
-public class MapActivity extends BaseActivity implements com.example.shichengxinag.monitorsystem.view.MapView{
+public class MapActivity extends BaseActivity implements com.example.shichengxinag.monitorsystem.view.MapView {
 
     @BindView(R.id.mapView)
     MapView mMapView;
+    @BindView(R.id.rootView)
+    View rootView;
     AMap mAMap;
+    private PopupWindow menuWindow;
     private MapPresenter mMapPresenter;
 
     @Override
@@ -31,7 +42,7 @@ public class MapActivity extends BaseActivity implements com.example.shichengxin
         mMapView.onCreate(savedInstanceState);
         if (mAMap == null)
             mAMap = mMapView.getMap();
-        mMapPresenter=new MapPresenter(this);
+        mMapPresenter = new MapPresenter(this);
         mMapPresenter.initMyLocation(mAMap);
         mMapPresenter.initUISetting(mAMap);
 
@@ -41,6 +52,15 @@ public class MapActivity extends BaseActivity implements com.example.shichengxin
     protected void onResume() {
         super.onResume();
         mMapView.onResume();
+    }
+
+    @OnClick({R.id.tv_msg})
+    public void onClickEvent(View view) {
+        switch (view.getId()) {
+            case R.id.tv_msg:
+                displayMenuWindow();
+                break;
+        }
     }
 
     @Override
@@ -53,6 +73,19 @@ public class MapActivity extends BaseActivity implements com.example.shichengxin
     protected void onDestroy() {
         super.onDestroy();
         mMapView.onDestroy();
+    }
+
+    private void displayMenuWindow() {
+        if (menuWindow == null) {
+            View view = LayoutInflater.from(this).inflate(R.layout.layout_menu, null);
+            menuWindow = new PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            menuWindow.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#88000000")));
+            menuWindow.setOutsideTouchable(true);
+            menuWindow.setAnimationStyle(R.style.popwindow_anim_left);
+        }
+        if (!menuWindow.isShowing())
+            menuWindow.showAtLocation(rootView, Gravity.CENTER_VERTICAL | Gravity.RIGHT, 0, 0);
+
     }
 
 
